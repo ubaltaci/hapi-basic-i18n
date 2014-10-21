51593bffaeb4dd09960e24d1cc90e66b7111a257
Uğur Baltacı <me@ugurbaltaci.com>
2014-10-21 16:13:14 +0300

    *

--- i18n.js
+++ i18n.js
@@ -15,9 +15,15 @@ module.exports = function(LANG_CODE, locale_path) {
     var lang_file = require(path.join(locale_path, LANG_CODE.toLocaleLowerCase() + ".js"));
 
     return function(STRING_CODE, REPLACEMENTS) {
+
+        if (!((typeof STRING_CODE == "string") || (STRING_CODE instanceof String))) {
+            return JSON.stringify(STRING_CODE);
+        }
+
         if ( REPLACEMENTS && !Array.isArray(REPLACEMENTS) ) {
             REPLACEMENTS = [REPLACEMENTS];
         }
+
         if ( lang_file[STRING_CODE] ) {
             return formatLocalization(lang_file[STRING_CODE], REPLACEMENTS);
         }
