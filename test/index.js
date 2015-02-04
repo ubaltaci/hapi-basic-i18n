b309c40f849cfd1271d1475307cc81e95ecb9cc1
Cagdas Akar <cagdas.akar@gmail.com>
2015-02-04 15:19:44 +0200

    version 1.0 with hapi 8 support

--- test/index.js
+++ test/index.js
@@ -49,7 +49,7 @@ describe("Hapi Basic i18n", function () {
                         expect(request.i18n).to.exist();
                         var localized = {
                             hello: request.i18n("Hello"),
-                            say_hello_to: request.i18n("Say Hello To", "Cagdas"),
+                            say_hello_to: request.i18n("Say Hello To", "Isaac"),
                             number: request.i18n("1"),
                             number_not_exist: request.i18n(198),
                             xxx: request.i18n("XXX"),
@@ -65,7 +65,7 @@ describe("Hapi Basic i18n", function () {
                 expect(res.result.localized).to.exist();
                 var localized = res.result.localized;
                 expect(localized.hello).to.equal("Hello!");
-                expect(localized.say_hello_to).to.equal("Hello Cagdas!");
+                expect(localized.say_hello_to).to.equal("Hello Isaac!");
                 expect(localized.number).to.equal("Number 1");
                 expect(localized.xxx).to.equal("EN XX");
                 expect(localized.number_not_exist).to.equal("198");
@@ -129,7 +129,7 @@ describe("Hapi Basic i18n", function () {
                         expect(request.i18n).to.exist();
                         var localized = {
                             hello: request.i18n("Hello"),
-                            say_hello_to: request.i18n("Say Hello To", "Cagdas"),
+                            say_hello_to: request.i18n("Say Hello To", "Isaac"),
                             number: request.i18n("1"),
                             xxx: request.i18n("XXX"),
                             not_exist: request.i18n("Hohoho")
@@ -140,12 +140,11 @@ describe("Hapi Basic i18n", function () {
             });
 
             server.inject("/", function (res) {
-                console.log(res.result);
                 expect(res.result).to.exist();
                 expect(res.result.localized).to.exist();
                 var localized = res.result.localized;
                 expect(localized.hello).to.equal("Merhaba!");
-                expect(localized.say_hello_to).to.equal("Selam Cagdas!");
+                expect(localized.say_hello_to).to.equal("Selam Isaac!");
                 expect(localized.number).to.equal("1 Numara");
                 expect(localized.xxx).to.equal("XXX");
                 expect(localized.not_exist).to.equal("Hohoho");
@@ -191,7 +190,7 @@ describe("Hapi Basic i18n", function () {
                 var tokens = res.result.split("\n");
 
                 expect(tokens[2]).to.equal("Hello!");
-                expect(tokens[3]).to.equal("Hello Cagdas!");
+                expect(tokens[3]).to.equal("Hello Isaac!");
                 done();
             });
         });
